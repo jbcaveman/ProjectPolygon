fef162a7f1960aabbef89ce06d329907dfc48e3e
Joel Cave <joel@cavemanlabs.com>
2023-04-07 20:19:57 -0500

    initial challenge working on desktop and mobile

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine.U2D;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private float doubleClickTolerance;
+    private bool alreadyClicked = false;
+
+    #region Lifecycle
+    void Update()
+    {
+        if (DoubleClickDetected())
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            DetectHitAt(mousePos2D);
+        } 
+
+        if (DoubleTapDetected())
+        {
+            Debug.Log("DoubleTapDetected");
+            Touch touch = Input.GetTouch(0);
+            DetectHitAt(touch.position);
+        }
+    }
+    #endregion
+
+    #region InputDetection
+    private bool DoubleClickDetected()
+    {
+        if (Input.GetMouseButtonDown(0))
+            if (alreadyClicked) return true;
+            else
+            {
+                StartCoroutine(WaitForDoubleClick());
+                return false;
+            }
+        else return false;
+    }
+
+    private bool DoubleTapDetected()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log("Tap Detected");
+
+            if (Input.GetTouch(0).tapCount == 2) return true;
+            else return false;
+        }
+        else return false;
+    }
+
+    private IEnumerator<WaitForSeconds> WaitForDoubleClick()
+    {
+        alreadyClicked = true;
+        yield return new WaitForSeconds(doubleClickTolerance);
+        alreadyClicked = false;
+    }
+
+    private void DetectHitAt(Vector2 position)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+        if (hit.collider != null)
+        {
+            SetRandomColorFor(hit.collider.GetComponent<SpriteShapeRenderer>().material);
+        }
+    }
+    #endregion
+
+    #region Actions
+    private void SetRandomColorFor(Material material)
+    {
+        if (material == null) return;
+
+        Color randomColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+        material.color = randomColor;
+    }
+    #endregion
+}
